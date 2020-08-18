9682964fcfe8059bd3875fd3e65cad804aaff254
ajiir <ajiir2247@gmail.com>
2020-08-19 03:34:24 +0800

    Finance modulruu item add hiideg bolloo

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ var uiController = (function () {
 // Санхүүтэй ажиллах
 
 var financeController = (function () {
+  // private date
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
@@ -36,8 +37,10 @@ var financeController = (function () {
     this.value = value;
   };
 
+  // private data
+
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: [],
     },
@@ -47,14 +50,39 @@ var financeController = (function () {
       exp: 0,
     },
   };
+
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+
+      data.items[type].push(item);
+    },
+
+    seeData: function () {
+      return data;
+    },
+  };
 })();
 
 // Программын холбогч
 var appController = (function (uiController, financeController) {
   var ctrlAddItem = function () {
     // 1.Оруулах өгөдлийг дэлгэцээс олж авна.
-    console.log(uiController.getInput());
+    var input = uiController.getInput();
+
     // 2. Олж авсан өгөдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+    financeController.addItem(input.type, input.description, input.value);
     // 3. Олж авсан өгөдлүүдээ вэб дээрээ тохирох хэсэг дээр гаргана.
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
